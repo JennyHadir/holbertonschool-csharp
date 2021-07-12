a62344f10c91da1b7164d708d9e6b03560395a58
Hadir Jenni <2063@holbertonschool.com>
2021-07-12 20:38:23 +0100

    ADD

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write(str);
+		Console.Write(str);
+        Console.WriteLine(str);
+        Console.Write(str.Substring(0, 9));
+        }
+}
