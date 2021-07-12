5cf1845ca452c21b2a7a181792d2e943e2e1d6e6
Hadir Jenni <2063@holbertonschool.com>
2021-07-12 22:46:38 +0100

    ADD

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Environment.ExitCode = 1;
+            Console.Error.Close();
+        }
+    }
+}
