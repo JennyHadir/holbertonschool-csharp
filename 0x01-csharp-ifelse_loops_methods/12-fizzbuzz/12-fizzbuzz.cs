6010b614b9d4a6660ecaa418282a954b670161ac
Hadir Jenni <2063@holbertonschool.com>
2021-07-13 21:06:59 +0100

    ADD

--- 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -6,7 +6,22 @@ namespace _12_fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i;
+            for (i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                    Console.Write("FizzBuzz");
+                else if (i % 5 == 0)
+                    Console.Write("Buzz");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz");
+                else
+                    Console.Write(i);
+                if (i != 100)
+                    Console.Write(" ");
+                else
+                    Console.WriteLine();
+            }
         }
     }
 }
