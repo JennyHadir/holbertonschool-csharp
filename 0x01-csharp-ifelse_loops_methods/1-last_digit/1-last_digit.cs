93fcfe7dabf9c18f19fec1ac95178103ad3d2af9
Hadir Jenni <2063@holbertonschool.com>
2021-07-13 21:11:03 +0100

    ADD

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -13,7 +13,7 @@ class Program
         }
         if (last_digit == 0)
         {
-            Console.WriteLine("The last digit of {0} is {1} and is 0 ", number, last_digit);
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, last_digit);
         }
         else if (last_digit > 5)
         {
