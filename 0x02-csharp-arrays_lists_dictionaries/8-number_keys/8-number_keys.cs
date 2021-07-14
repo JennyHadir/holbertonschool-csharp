24d16c74862483b89cf863d2035be6b97a599295
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 21:44:08 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+
+        int countKey = 0;
+        foreach (var item in myDict)
+            countKey++;
+        return countKey;
+    }
+}
