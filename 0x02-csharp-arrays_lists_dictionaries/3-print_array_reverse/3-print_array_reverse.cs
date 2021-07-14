fa0b433b09e173d8cb31112d0f696c5dac99a6a1
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 15:10:35 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+                Console.Write(array[i]);
+                if (i != 0)
+                    Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
