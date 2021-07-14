66ca055518617cc080bfd3b129961b549a617f73
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 14:20:15 +0100

    Update

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,28 +2,22 @@
 
 class Array
 {
-        public static int[] CreatePrint(int size)
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
         {
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
-            int [] narray = new int [size];
-            if (size == 0)
-            {
-                Console.WriteLine();
-                return narray;
-            }
-            else
-            {
-                for (int i = 0; i < narray.Length; i++)
-                {
-                    narray[i] = i;
-                    Console.Write(narray[i] + " ");
-                }
-                Console.WriteLine();
-                return narray;
-            }
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+        int [] narray = new int [size];
+        for (int i = 0; i < narray.Length; i++)
+        {
+            narray[i] = i;
+            Console.Write(narray[i]);
+            if (i != size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return narray;
+    }
 }
