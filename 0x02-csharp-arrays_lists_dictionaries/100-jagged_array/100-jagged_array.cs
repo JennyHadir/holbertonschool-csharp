d1f51222c51df1c093dfde247b851f66c38623fe
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 23:56:33 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[3][];
+            arr[0] = new int[] { 0, 1, 2, 3 };
+            arr[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            arr[2] = new int[] { 0, 1 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
