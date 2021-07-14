ea6ffc3cdd1afe4fdee46d1a640faef60f4b1efe
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 23:46:00 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] multiArray = new int [5, 6];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    multiArray[i, j] = 0;
+                }
+            }
+            multiArray[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (j != 5)
+                        Console.Write(multiArray[i, j] + " ");
+                    else Console.WriteLine();
+                }
+            }
+        }
+    }
+}
