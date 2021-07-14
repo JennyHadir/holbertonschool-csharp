d79c6df5a4bfa8ad52120d7b0c12d1dda9d4aa54
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 22:02:11 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        foreach (var item in myDict)
+        {
+            if (item.Key == key)
+            {
+                myDict.Remove(key);
+                break;
+            }
+        }
+        return myDict;
+    }
+}
