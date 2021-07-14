be00f9d12c95f9a09e980a7fb95ec9c451180566
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 22:41:30 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var list = myDict.Keys.ToList();
+        list.Sort();
+        foreach (var key in list)
+            Console.WriteLine("{0}: {1}", key, myDict[key]);
+    }
+}
