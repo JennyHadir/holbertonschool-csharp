fd5ad8fd8f902189f8a8c244f9d408e8d1014175
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 23:16:27 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = -1;
+        string biggest = "None";
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > max)
+                {
+                    max = entry.Value;
+                    biggest = entry.Key;
+                }
+        }
+        return biggest;
+    }
+}
