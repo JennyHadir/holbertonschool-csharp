7b833ebbe00bde5943ff8e8d8b4f12fcb025d870
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 15:55:48 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+           Console.WriteLine("List is empty");
+           return -1;
+        }
+        int max = myList[0];
+        for (int index = 0; index < myList.Count; index++)
+        {
+            if (max < myList[index])
+                max = myList[index];
+        }
+        return max;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> VerifList = new List<bool>();
+        for (int index = 0; index < myList.Count; index++)
+        {
+            if (myList[index] % 2 == 0)
+                VerifList.Add(true);
+            else
+                VerifList.Add(false);
+        }
+        return VerifList;
+    }
+}
