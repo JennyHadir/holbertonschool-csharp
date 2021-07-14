d88795032e13c538fe2d10573e9c03ca45d47673
Hadir Jenni <2063@holbertonschool.com>
2021-07-14 16:04:33 +0100

    ADD

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index > myList.Count || index < 0)
+            Console.WriteLine("Index out of range");
+        else
+            myList.Remove(myList[index]);
+        return myList;
+    }
+}
