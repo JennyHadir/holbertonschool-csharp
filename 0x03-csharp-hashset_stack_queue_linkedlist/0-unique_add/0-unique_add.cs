6026535c9392894d40b5f3bd27859b200dcfb250
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 14:59:53 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int all = 0;
+        List<int> uniqueList = myList.Distinct().ToList();
+        foreach (var item in uniqueList)
+        {
+            all = all + item;
+        }
+        return all;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,8 @@
+Learning Objectives
+How to create and use arrays
+What are Collections in C#?
+How to create and use hashsets
+How to create and use stacks
+How to create and use queues
+How to create and use linked lists
+How to use foreach
