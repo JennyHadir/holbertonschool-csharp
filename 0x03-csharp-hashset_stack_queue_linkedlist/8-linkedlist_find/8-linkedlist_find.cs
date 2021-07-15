11c5e776ee68b36f4e7aad3a90e9f604e7a4b49b
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 21:16:05 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = -1;
+        if (myLList.Contains(value))
+        {
+            index++;
+            foreach (var item in myLList)
+            {
+                if (item == value)
+                    break;
+                index++;
+            }
+        }
+        return index;
+    }
+}
