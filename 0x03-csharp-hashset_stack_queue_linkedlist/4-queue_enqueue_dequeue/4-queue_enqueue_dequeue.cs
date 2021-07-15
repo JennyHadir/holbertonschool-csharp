cdef5065a41fec5a0fb60dbd5a64d09b15b43f03
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 20:17:06 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: " + aQueue.Peek());
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        while (aQueue.Contains(search))
+            aQueue.Dequeue();
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
+
