0a9235af5ffae734bd85cc66b7bd7c069499ea07
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 21:06:03 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int counter = 0;
+        foreach (var item in myLList)
+            counter++;
+        return counter;
+    }
+}
