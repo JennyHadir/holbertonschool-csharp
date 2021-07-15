9ed13a4ace30e8ff454ef4155a9e186428b8ea0e
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 22:28:56 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int num = 0;
+        foreach (var item in myLList)
+        {
+            num = num + item;    
+        }
+        return num;
+    }
+}
