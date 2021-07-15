55206227803ba03a2bf2d0ef65895355efab33f3
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 23:15:30 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        if (myLList.First == null || myLList.First.Value >= n)
+        {
+            myLList.AddFirst(n);
+            return myLList.First;
+        }
+        LinkedListNode<int> node = myLList.First;
+        while (node.Next != null)
+        {
+            if (node.Next.Value >= n)
+            {
+                node = myLList.AddAfter(node, n);
+                return node;
+            }
+            node = node.Next;
+        }
+        node = myLList.AddLast(n);
+        return node;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int counter = 0;
+        foreach (var item in myLList)
+        {
+            if (index == counter)
+            {
+                myLList.Remove(item);
+                return;
+            }
+            counter++;
+        }
+    }
+}
