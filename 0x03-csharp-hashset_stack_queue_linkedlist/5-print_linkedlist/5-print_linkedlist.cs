457fd66c4f273a926cceff5765a5af3bf6d203bb
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 20:38:46 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        int i;
+        LinkedList<int> linked = new LinkedList<int>();
+        if (size > 0)
+        {
+            for (i = 0; i < size; i++)
+            {
+                linked.AddLast(i);
+            }
+            foreach (var item in linked)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        return linked;
+    }
+}
