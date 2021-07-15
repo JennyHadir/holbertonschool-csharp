65ec6e315f0fbd693631ac80425a753570104cb3
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 22:06:22 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int data = 0;
+        if (myLList.First != null)
+        {
+            data = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+        return data;
+    }
+}
