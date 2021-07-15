54924b0dc130011db67f28bb3256df1a06c3ada5
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 15:38:38 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> nList = list1;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (!list2.Contains(list1[i]))
+                nList.Remove(list1[i]);
+        }
+        return nList;
+    }
+}
