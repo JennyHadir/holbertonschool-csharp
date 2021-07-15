e1e5760d5d77900f57cbf688f819ef9abc8e59ae
Hadir Jenni <2063@holbertonschool.com>
2021-07-15 22:15:59 +0100

    ADD

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int nodeData = 0;
+        if (n < myLList.Count && n > 0)
+        {
+            nodeData = myLList.ElementAt(n);
+        }
+        return nodeData;
+    }
+}
