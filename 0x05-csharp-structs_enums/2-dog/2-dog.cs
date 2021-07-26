8f290799a43abbefefe8382b0d3a798831a6872d
Hadir Jenni <2063@holbertonschool.com>
2021-07-26 23:07:29 +0100

    Add

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,21 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog{
+public string name;
+public float age;
+public string owner;
+public Rating rating;   
+
+public Dog(string n, float a, string o, Rating r)
+{
+    name = n;
+    age = a;
+    owner = o;
+    rating = r;
+}
+}
