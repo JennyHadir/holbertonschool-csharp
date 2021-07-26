3515923d60f11b7a2b956dc57d2f16dc6d4daac8
Hadir Jenni <2063@holbertonschool.com>
2021-07-26 20:12:57 +0100

    Add

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            for (; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+        }
+        catch (System.Exception)
+        {
+            
+        }
+        return i;
+    }
+}
+
