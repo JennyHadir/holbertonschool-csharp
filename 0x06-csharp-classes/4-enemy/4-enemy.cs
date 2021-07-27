667b75cf6eebc376d1c343292f9190ee8764338d
Hadir Jenni <2063@holbertonschool.com>
2021-07-27 14:26:29 +0100

    Add

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie Class.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health.
+        /// </summary>
+        public int health;
+        private string name = "(No name)";
+
+        /// <summary> gets or sets the name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// <param name="value"> The value of health.</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        /// <summary>
+        /// Return the value of health of the zombie
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
