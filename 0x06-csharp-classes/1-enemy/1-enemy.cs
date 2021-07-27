75973406f2320856aa6c66966f26669f6e3f4ed4
Hadir Jenni <2063@holbertonschool.com>
2021-07-27 13:44:18 +0100

    Add

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie Class.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+    }
+}
