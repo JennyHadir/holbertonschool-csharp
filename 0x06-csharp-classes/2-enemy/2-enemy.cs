afdec2632eb2c0078b9587816fdd6f3e069948f3
Hadir Jenni <2063@holbertonschool.com>
2021-07-27 14:00:06 +0100

    Add

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty Zombie Class.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// <param name="value"> The value of health.</param>
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
