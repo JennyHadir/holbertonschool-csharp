efb0a0f8e471e486f9d8450bdc29d3e2f79d8f5b
Hadir Jenni <2063@holbertonschool.com>
2021-07-29 17:56:44 +0100

    Add

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        ///<summary>Test simple division by 2</summary>
+        public void TestDivision()
+        {
+            int[,] matrix = new int[,] { { 2, 5 }, { 7, 4 }, { 11, 6 }, { 17, 14 } };
+            int num = 2;
+            int[,] NewMatrix = MyMath.Matrix.Divide(matrix, num);
+            int[,] CheckMatrix = new int[,] { { 1, 2 }, { 3, 2 }, { 5, 3 }, { 8, 7 } };
+            Assert.AreEqual(CheckMatrix, NewMatrix);
+        }
+
+        [Test]
+        ///<summary>Test Divisions by zero</summary>
+        public void TestDivisionByZero()
+        {
+            int[,] matrix = new int[,] { { 2, 4 }, { 8, 4 }, { 10, 6 }, { 12, 14 } };
+            int num = 0;
+            int[,] NewMatrix = MyMath.Matrix.Divide(matrix, num);
+            Assert.AreEqual(null, NewMatrix);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary> Matrix class </summary>
+    public class Matrix
+    {
+        /// <summary> Divise a all matrix element by integer </summary>
+        /// <param name="matrix"> matrix to divise its elements </param>
+        /// <param name="num"> divisor </param>
+        /// <returns> new matrix contains the division result </returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix.Length == 0)
+                return null;
+            try
+            {
+                int[,] NewMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        NewMatrix[i, j] = (matrix[i, j] / num);
+                    }
+
+                }
+                return NewMatrix;
+            }
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+        }
+    }
+}
