77087dbfcce7c86e07a1125ab7138707cb170f94
Hadir Jenni <2063@holbertonschool.com>
2021-07-31 23:36:24 +0100

    Update

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -4,12 +4,12 @@ using System.Linq;
 
 namespace MyMath
 {
-    ///<summary> Operations Class </summary>
+    ///<summary> Operations Class.</summary>
     public class Operations
     {
-        ///<summary> Get the max value in the list</summary>
-        ///<param name="nums"> List to loop through</param>
-        /// <returns> maximum value in the list or 0 if empty</returns>
+        ///<summary> Get the max value in the list.</summary>
+        ///<param name="nums"> List to loop through.</param>
+        /// <return> maximum value in the list or 0 if empty.</return>
         public static int Max(List<int> nums)
         {
             if (nums.Count == 0)
