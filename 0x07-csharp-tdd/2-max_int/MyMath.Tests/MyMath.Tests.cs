a548c139a278b48614d9854ce1a11efb8b98c864
Hadir Jenni <2063@holbertonschool.com>
2021-07-31 22:03:52 +0100

    Add

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestEmptyList()
+        {
+            List<int> ls = new List<int>();
+            Assert.AreEqual(0, MyMath.Operations.Max(ls));
+        }
+
+        [Test]
+        public void TestPositiveNegativeList()
+        {
+            List<int> ls = new List<int>();
+            ls.Add(0);
+            ls.Add(-1);
+            ls.Add(6);
+            ls.Add(2);
+            ls.Add(-3);
+            Assert.AreEqual(6, MyMath.Operations.Max(ls));
+        }
+
+        [Test]
+        public void TestPositiveList()
+        {
+            List<int> ls = new List<int>();
+            ls.Add(0);
+            ls.Add(1);
+            ls.Add(6);
+            ls.Add(2);
+            ls.Add(3);
+            Assert.AreEqual(6, MyMath.Operations.Max(ls));
+        }
+
+        [Test]
+        public void TestNegativeList()
+        {
+            List<int> ls = new List<int>();
+            ls.Add(-1);
+            ls.Add(-6);
+            ls.Add(-2);
+            ls.Add(-3);
+            Assert.AreEqual(-1, MyMath.Operations.Max(ls));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
+{
+    ///<summary> Operations Class </summary>
+    public class Operations
+    {
+        ///<summary> Get the max value in the list</summary>
+        ///<param name="nums"> List to loop through</param>
+        /// <returns> maximum value in the list or 0 if empty</returns>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+                return 0;
+            int max = nums.First();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > max)
+                    max = nums[i];
+            }
+            return (max);
+        }
+    }
+}
