83198b106a108e181858bcf9345358b616558431
Hadir Jenni <2063@holbertonschool.com>
2021-08-03 00:06:47 +0100

    Add

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+///<summary> Obj class</summary>
+class Obj
+{
+    ///<summary> List all</summary>
+    ///<param name="myObj">Object to list their props and methods</param>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] props = myObj.GetType().GetProperties();
+        MethodInfo[] methods = myObj.GetType().GetMethods();
+
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach(var prop in props)
+        {
+            Console.WriteLine(prop.Name);
+        }
+
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (var method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
