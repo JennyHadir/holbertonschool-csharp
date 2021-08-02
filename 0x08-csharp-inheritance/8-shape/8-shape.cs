160b28afeb9aa6e24feced5548fcf9398a1e1710
Hadir Jenni <2063@holbertonschool.com>
2021-08-02 16:55:27 +0100

    [ADD]: init add

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+///<summary>Shape class</summary>
+class Shape
+{
+    ///<summary>Throw an exception</summary>
+    ///<return></return>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+///<summary>Rectangle Derived Class.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;   
+        }
+    }
+
+    ///<summary> Calculat the area</summary>
+    ///<return>Area</return>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    ///<summary> Override</summary>
+    ///<return>String representation of Rectangle</return>
+    public override string ToString()
+    {
+        return $"[Rectangle] {this.width} / {this.height}";
+    }
+}
+
+///<summary>Square class that inherits from Rectangle</summary>
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get
+        {
+            return size;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+                size = value;
+                Height = value;
+                Width = value;            
+        }
+    }
+}
