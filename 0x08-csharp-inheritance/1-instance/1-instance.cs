b0e99ad402377506c8cff77925a35f8a1af54fe2
Hadir Jenni <2063@holbertonschool.com>
2021-08-02 14:13:09 +0100

    Initial Add

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary> Obj class</summary>
+class Obj
+{
+    ///<summary> Check if obj is an instance or an instance of a class that inherited from Array</summary>
+    ///<param name="obj">Object to check</param>
+    ///<return>True if the object is an instance of, or if the object is an instance of a class that inherited from, Array, otherwise return False.</return>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        return false;
+    }
+}
+
