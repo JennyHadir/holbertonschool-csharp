894e392f2cf24950c0059f046945096aee0fc7a3
Hadir Jenni <2063@holbertonschool.com>
2021-08-02 16:15:47 +0100

    [ADD]: init add

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Shape class</summary>
+class Shape
+{
+    ///<summary>Throw an exception</summary>
+    ///<return></return>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
