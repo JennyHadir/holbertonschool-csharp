80cb14777c0f6a640cde32afefeb97ca7818f3d1
Hadir Jenni <2063@holbertonschool.com>
2021-08-29 23:34:54 +0100

    Update

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 ///<summary> MatrixMath class</summary>
 public class MatrixMath
@@ -10,9 +9,12 @@ public class MatrixMath
     ///<return> matrix result of the addition of two matrix </return>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] matrix3 = new double [matrix1.GetLength(0), matrix1.GetLength(1)]; 
         if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
         {
+            double[,] matrix3 = {
+                {0, 0},
+                {0, 0}
+            }; 
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix1.GetLength(1); j++)
@@ -24,6 +26,11 @@ public class MatrixMath
         }
         else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
         {
+            double[,] matrix3 = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            }; 
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix1.GetLength(1); j++)
@@ -33,7 +40,6 @@ public class MatrixMath
             }
             return matrix3;
         }
-        else
-            return new double[,] {{-1}};
+        return new double[,] {{-1}};
     }
 }
