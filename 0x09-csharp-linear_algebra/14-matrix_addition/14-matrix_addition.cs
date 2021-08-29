499c449e58de03658ee1169782a269dfa815207b
Hadir Jenni <2063@holbertonschool.com>
2021-08-29 23:42:24 +0100

    Update

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 
-///<summary> MatrixMath class</summary>
+///<summary>Class matrix math</summary>
 class MatrixMath
 {
-    ///<summary>Add method to add two matrix with the same size</summary>
-    ///<param name="matrix1"> matrix1</param>
-    ///<param name="matrix2"> matrix2</param>
-    ///<return> matrix result of the addition of two matrix </return>
-    public static double[,] Add(double[,] matrix1, double[,] matrix2)
-    {
-        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
-        {
-            double[,] matrix3 = {
-                {0, 0},
-                {0, 0}
-            }; 
-            for (int i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
-                {
-                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
-                }
-            }
-            return matrix3;
-        }
-        else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
-        {
-            double[,] matrix3 = {
-                {0, 0, 0},
-                {0, 0, 0},
-                {0, 0, 0}
-            }; 
-            for (int i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
-                {
-                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
-                }
-            }
-            return matrix3;
-        }
-        return new double[,] {{-1}};
-    }
+	///<summary>matrix addition</summary>
+	///<return>matrix or matrix containing -1</return>
+	public static double[,] Add(double[,] matrix1, double[,] matrix2)
+	{
+		if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
+		matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
+		{
+			double[,] matrix3 = {
+				{0, 0},
+				{0, 0}
+			};
+			matrix3[0, 0] = matrix1[0, 0] + matrix2[0, 0];
+			matrix3[0, 1] = matrix1[0, 1] + matrix2[0, 1];
+			matrix3[1, 0] = matrix1[1, 0] + matrix2[1, 0];
+			matrix3[1, 1] = matrix1[1, 1] + matrix2[1, 1];
+			return matrix3;
+		}
+		else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 &&
+		matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
+		{
+			double[,] matrix3 = {
+				{0, 0, 0},
+				{0, 0, 0},
+				{0, 0, 0}
+			};
+			matrix3[0, 0] = matrix1[0, 0] + matrix2[0, 0];
+			matrix3[0, 1] = matrix1[0, 1] + matrix2[0, 1];
+			matrix3[0, 2] = matrix1[0, 2] + matrix2[0, 2];
+			matrix3[1, 0] = matrix1[1, 0] + matrix2[1, 0];
+			matrix3[1, 1] = matrix1[1, 1] + matrix2[1, 1];
+			matrix3[1, 2] = matrix1[1, 2] + matrix2[1, 2];
+			matrix3[2, 0] = matrix1[2, 0] + matrix2[2, 0];
+			matrix3[2, 1] = matrix1[2, 1] + matrix2[2, 1];
+			matrix3[2, 2] = matrix1[2, 2] + matrix2[2, 2];
+			return matrix3;
+		}
+		return new double[,] { { -1 } };
+	}
 }
