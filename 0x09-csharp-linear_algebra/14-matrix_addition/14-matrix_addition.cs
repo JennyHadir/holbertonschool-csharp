370a0c2aa14ff79e6b2062dbe22ae4e51cdb0463
Hadir Jenni <2063@holbertonschool.com>
2021-08-29 17:26:31 +0100

    ADD

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _14_matrix_addition
+///<summary> MatrixMath class</summary>
+public class MatrixMath
 {
-    class Program
+    ///<summary>Add method to add two matrix with the same size</summary>
+    ///<param name="matrix1"> matrix1</param>
+    ///<param name="matrix2"> matrix2</param>
+    ///<return> matrix result of the addition of two matrix </return>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        static void Main(string[] args)
+        double[,] matrix3 = new double [matrix1.GetLength(0), matrix1.GetLength(1)]; 
+        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return matrix3;
         }
+        else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
+        {
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return matrix3;
+        }
+        else
+            return new double[,] {{-1}};
     }
 }
