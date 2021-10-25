f2de42ad04a3de463e986b4196e30884952dd0cc
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-25 23:56:33 +0100

    ADD

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+///<summary>  Create new class called Queue</summary>
+public class Queue<T>
+{
+    /// First Node
+    public Node head;
+    /// Last Node
+    public Node tail;
+    /// Number of Nodes
+    public int count;
+
+    /// Returns the Queue's Type
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    ///<summary>  Create new class called Node</summary>
+    public class Node
+    {
+        /// Node class props
+        T value = default(T);
+        /// Next Node in queue
+        public Node next = null;
+        /// Constructor
+        public Node(T val)
+        {
+            value = val;
+        }
+    }
+
+    /// Add a new node to the queue
+    public Node Enqueue(T val)
+    {
+        Node nNode = new Node(val);
+        
+        if (head == null)
+        {
+            head = nNode;
+        }
+        else
+        {
+            nNode.next = head;
+            tail = nNode;
+        }
+        count++;
+        return head;
+    }
+
+    /// Returns the nulmber of Nodes
+    public int Count()
+    {
+        return(count);
+    }
+
+}
