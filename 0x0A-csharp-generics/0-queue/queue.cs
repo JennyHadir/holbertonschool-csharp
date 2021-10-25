36bf65fc2d807eea82f61212c396a10781999b6e
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-25 23:41:13 +0100

    ADD

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>  Create new class called Queue</summary>
+public class Queue<T>
+{
+    /// Returns the Queue's Type
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
