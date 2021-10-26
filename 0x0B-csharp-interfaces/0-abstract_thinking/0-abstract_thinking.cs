cb2956333fc56a6994ca2b81669dec346a59efdd
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-26 14:05:59 +0100

    ADD

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// Abstract Base class
+public abstract class Base
+{
+    /// Name
+    public string name;
+
+    /// Override method
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType());
+    }
+
+}
+
