8f0b6d429e0916ab4143725bf1cb63f87d1870c8
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-27 23:45:52 +0100

    ADD

---
+++ 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+
+///<summary> Player class</summary>
+public class Player
+{
+    ///<summary> Player'spoperties</summary>
+    private string name;
+    private float maxHp, hp;
+
+    ///<summary>Player's Constructor</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp < 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.name = name;
+    }
+
+    ///<summary> Player's method to print his health</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + hp + " / " + maxHp + " health");
+    }
+
+    ///<summary>TakeDamage delegate method</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+        Console.WriteLine(name +" takes " + damage + " damage!");
+        this.hp -= damage;
+        ValidateHP(hp);
+    }
+
+    ///<summary>TakeDamage delegate method</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+        }
+        Console.WriteLine(name +" takes " + heal + " HP!");
+        this.hp += heal;
+        ValidateHP(hp);
+    }
+
+    ///<summary> CalculateHealth Delegate</summary>
+    public delegate void CalculateHealth(float damage);
+
+    ///<summary>ValidateHP method</summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            hp = 0;
+        }
+        else if (newHp > maxHp)
+        {
+            hp = maxHp;
+        }
+        else
+        {
+            hp = newHp;
+        }
+    }
+
+    ///<summary> ApplyModifier</summary>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if ( modifier == Modifier.Weak)
+        {
+            return (baseValue / 2);
+        }
+        else if (modifier == Modifier.Base)
+        {
+            return (baseValue);
+        }
+        else
+        {
+            return (1.5f * baseValue);
+        }
+    }
+}
+
+///<summary>Enum Modifier</summary>
+public enum Modifier
+{
+    /// Weak Modifier
+    Weak,
+    /// Base Modifier
+    Base,
+    /// Strong Modifier
+    Strong
+}
+
+///<summary> CalculateModifier</summary>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
