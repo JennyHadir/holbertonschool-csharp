5739908f3052fab13a0e9fd5a4b091bcaaff6120
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-27 21:01:48 +0100

    Add

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+///<summary> Player class</summary>
+public class Player
+{
+    ///<summary> Player'spoperties</summary>
+    private string name;
+    private float maxHp, hp;
+
+    ///<summary>Player's Constructor</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp < 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.name = name;
+    }
+
+    ///<summary> Player's method to print his health</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + hp + " / " + maxHp + " health");
+    }
+}
+
