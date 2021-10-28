926bbfde70d815f156a74a5c7c0dbcba39e22f32
Hadir Jenni <hadir.jenni@gmail.com>
2021-10-28 13:20:26 +0100

    Update

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -6,25 +6,26 @@ public class Player
 {
     ///<summary> Player'spoperties</summary>
     private string name;
-    private float maxHp, hp;
+    private float maxHp;
+    private float hp;
 
     ///<summary>Player's Constructor</summary>
     public Player(string name="Player", float maxHp=100f)
     {
-        if (maxHp <= 0)
+        this.name = name;
+        if (maxHp <= 0f)
         {
-            this.maxHp = 100f;
+            maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
         this.maxHp = maxHp;
         this.hp = maxHp;
-        this.name = name;
     }
 
     ///<summary> Player's method to print his health</summary>
     public void PrintHealth()
     {
-        Console.WriteLine(this.name + " has " + hp + " / " + maxHp + " health");
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
     }
 }
 
